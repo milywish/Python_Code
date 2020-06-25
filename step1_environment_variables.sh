1bf9fe42a1b89241825d352b03e471bfc2379368
deloittesentimentpoc11 <67398619+deloittesentimentpoc11@users.noreply.github.com>
2020-06-24 20:34:11 -0400

    Create step1_environment_variables.sh

---
+++ step1_environment_variables.sh
@@ -0,0 +1,7 @@
+echo "# Python_Code" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Deloitte-GCP-USDC-Developers/Python_Code.git
+git push -u origin master
+                
